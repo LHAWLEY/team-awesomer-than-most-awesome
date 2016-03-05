1b55b06b851bb44b106916079e7571fc9795fb9c
Ian Kinner <email": null>
2016-03-05 12:01:40 -0600

    fix seeds

--- db/guess_round-seed.rb
+++ db/guess_round-seed.rb
@@ -1,11 +1,11 @@
 module GuessRoundSeed
   3.times do |x|
-    Round.create({deck_id: 1, user_id: x})
+    Round.create({deck_id: 1, user_id: x+1})
   end
 
   3.times do |x|
     round = rand(1..3)
-    deck = Deck.find(x+4)
+    deck = Deck.find(x+1)
     deck.cards.each do |card|
       Guess.create({card_id: card.id, round_id: round, status: true})
     end
